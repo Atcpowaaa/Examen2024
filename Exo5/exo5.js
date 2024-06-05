1f3239627f9b833a10805ffb92828d78ca4503af
Atcpowaaa <152889856+Atcpowaaa@users.noreply.github.com>
2024-06-05 13:55:59 +0000

    Exo5 Fonctionnel

---
+++ Exo5/exo5.js
@@ -0,0 +1,8 @@
+const stra = '2929123456789';
+const numbers = stra.split('');
+console.log(numbers);
+const carre = numbers.map((x) => x * x);
+console.log(carre);
+console.log(carre.join(''));
+
+
