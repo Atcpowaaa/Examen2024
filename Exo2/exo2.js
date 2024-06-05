825f6e774854941c7ea26b27ad57c00000da19b3
Atcpowaaa <152889856+Atcpowaaa@users.noreply.github.com>
2024-06-05 15:26:46 +0000

    Test non concluant exo2

--- Exo2/exo2.js
+++ Exo2/exo2.js
@@ -1,13 +1,20 @@
-Var list1 = [
-    {FirstName: 'Noah', lastName: 'M.', pays: 'Suisse', continent: 'Europe', age: 19, langue: 'C'
+const list1 = [
+    {FirstName: 'Noah', lastName: 'M.', pays: 'Suisse', continent: 'Europe', age: 19, langue: 'C',
     Repas: 'végétarien'},
     {FirstName: 'Anna', lastName: 'R.', pays: 'Liechtenstein', continent: 'Europe', age: 52, langue:
     'JavaScript',
     Repas: 'standard'},
     {FirstName: 'Ramona', lastName: 'R.', pays: 'Paraguay', continent: 'Amériques', age: 29, langue: 'Ruby',
     Repas: 'vegan'},
-    {FirstName: 'George', lastName: 'B.', pays: 'Angleterre', continent: 'Europe', age: 81, langue: 'C'
+    {FirstName: 'George', lastName: 'B.', pays: 'Angleterre', continent: 'Europe', age: 81, langue: 'C',
     Repas: 'végétarien'},
     ];
 
+
+      
+      function regime(repas) {
+        return  repas === "végétarien";
+      }
+      
+      console.log(list1.find(regime));
     
